26ad81819849a02b77a7a984186e00be1e0ee7de
maxgsandre <mgsa.gustavo@gmail.com>
2023-10-04 15:25:39 -0300

    chore: inicializa o projeto Express com rotas e middleware JSON

---
+++ src/routes.js
@@ -0,0 +1,9 @@
+const { Router } = require ("express")
+
+const routes = new Router()
+
+routes.get('/', (request, response) => {
+    return response.json({message: "Helo World"})
+})
+
+module.exports = routes
